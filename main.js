571ac5902e4166409feb4d23ff9826c6f76582fe
AzamatTash <atashinov98@mail.ru>
2021-11-01 19:31:58 +0500

    refactoring calculator

--- main.js
+++ main.js
@@ -1,83 +1,24 @@
-// Переменные---------------------------------------
-
-let x = prompt('Введите число');
-let a = prompt('Введите опирацию');
-let y = prompt('Введите число');
-
-// Функция для проверки вычесляемых значений-------
-
-function checkValue(a,x,y) {
-
-    if (a == '' || x == '' || y == '') {
-        return false;
-    } else if (!a || !x || !y) {
-        return false;
-    } else {
-        return true;
-    }
-
-}
-
-// Функция калькулятора-----------------------------
-
 function calc(a,x,y) {
 
-    if (checkValue(a,x,y)) {
-
-        if (a == '^') {
-            alert('итог : ' + pow(x,y));
-        } else if (a == '+') {
-            alert('итог : ' + sum(x,y));
-        } else if (a == '-') {
-            alert('итог : ' + sub(x,y));
-        } else if (a == '*') {
-            alert('итог : ' + multi(x,y));
-        } else if (a == '/') {
-            alert('итог : ' + div(x,y));
-        } else if (a == '%') {
-            alert('итог : ' + remDiv(x,y));
-        } else {
-            alert('Unknown operation');
-        }   
-
+    if (x == undefined || !y || (typeof x !== 'number') || (typeof y !== 'number')) {
+        result = 'Error'
+    } else if (a === 'pow') {
+        result = x ** y;
+    } else if (a === 'sum') {
+        result = x + y;
+    } else if (a === 'sub') {
+        result = x - y;
+    } else if (a === 'multi') {
+        result = x * y;
+    } else if (a === 'div') {
+        result = x / y;
+    } else if (a === 'remDiv') {
+        result = x % y;
     } else {
-        alert ('Error');
+        result = "Unknown operation"
     }
-   
+    return result;
+    
 }
 
-// Вызов функции калькулятора-----------------------
-
-calc(a,x,y);
-
-// Математические опирации---------------------------
-
-// Возведение в степень
-function pow(x,y) {
-    return x ** y;   
-}
-
-// Сумма
-function sum(x,y) {
-    return +x + +y;   
-}
-
-// Вычитание
-function sub(x,y) {
-    return x - y;   
-}
-
-// Умножение
-function multi(x,y) {
-    return x*y;   
-}
-
-// Деление
-function div(x,y) {
-    return x / y;
-}
-
-// Остаток от деления
-function remDiv(x,y) {
-    return x % y;   
-}
+console.log(calc('sum',1,1));
