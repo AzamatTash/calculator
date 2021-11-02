1af0ec254616c2e8dc97cd7a64708bf0a243fd54
AzamatTash <atashinov98@mail.ru>
2021-11-02 22:13:31 +0500

    switch case

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
 function calc(a,x,y) {
 
-    if (x == undefined || !y || (typeof x !== 'number') || (typeof y !== 'number')) {
-        result = 'Error'
-    } else if (a === 'pow') {
-        result = x ** y;
-    } else if (a === 'sum') {
-        result = x + y;
-    } else if (a === 'sub') {
-        result = x - y;
-    } else if (a === 'multi') {
-        result = x * y;
-    } else if (a === 'div') {
-        result = x / y;
-    } else if (a === 'remDiv') {
-        result = x % y;
-    } else {
-        result = "Unknown operation"
+    const isNotNumber = typeof x !== 'number' || typeof y !== 'number'; 
+    const isNotValid = x == undefined || !y ;
+
+    if (isNotNumber || isNotValid) {
+        return 'Error';
+    }       
+    
+    switch (a)  {
+        case 'pow':
+            return x ** y;
+            break;
+        case 'sum':
+            return x + y;
+            break;
+        case 'sub':
+            return x - y;
+            break;
+        case 'multi':
+            return x * y;
+            break;
+        case 'div':
+            return x / y;
+            break;
+        case 'remDiv':
+            return x % y;
+            break;
+        default:
+            return "Unknown operation";
+            break;
     }
-    return result;
     
 }
 
-console.log(calc('sum',1,1));
+console.log(calc('iv',10,10));
