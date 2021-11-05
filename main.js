8952b5a0b172948c130f171d82e723b39f2b33d4
AzamatTash <atashinov98@mail.ru>
2021-11-05 12:35:40 +0500

    Object

--- main.js
+++ main.js
@@ -1,40 +1,23 @@
 function calc(a,x,y) {
-
     const isNotNumber = typeof x !== 'number' || typeof y !== 'number'; 
-   
+        
+    let operations = {
+        sum: x + y,
+        sub: x - y,
+        multi: x * y,
+        div: y == 0 ? 'Error' : x / y,
+        pow: x ** y,
+        remDiv: x % y,
+    }
+
     if (isNotNumber) {
         return 'Error';
-    }       
-    
-    switch (a)  {
-        case 'pow':
-            return x ** y;
-            break;
-        case 'sum':
-            return x + y;
-            break;
-        case 'sub':
-            return x - y;
-            break;
-        case 'multi':
-            return x * y;
-            break;
-        case 'div':
-            switch (y) {
-                case 0:
-                    return 'Error';
-                    break;
-            }
-            return x / y;
-            break;
-        case 'remDiv':
-            return x % y;
-            break;
-        default:
-            return "Unknown operation";
-            break;
+    } else if (a in operations) {
+        return operations[a];
+    } else {
+        return 'unknown operation';
     }
-    
-}
+} 
+
 
 console.log(calc('div',10,0));
